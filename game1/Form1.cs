593dc3206aa239b8163bce1a1ca97538cd775d88
Zhang <feng.d.zhang@accenture.com>
2019-11-07 16:33:36 +0800

    添加项目文件。

---
+++ game1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using game1.func;
+
+namespace game1
+{
+    public partial class Form1 : Form
+    {
+        ArrayList stars;
+        Graphics g;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Rendorter.isRun = !Rendorter.isRun;
+            button1.Text = Rendorter.isRun.ToString();
+        }
+
+
+        private void NewMethod(PaintEventArgs e)
+        {
+            SolidBrush sb;
+            Rectangle r;
+            foreach (Star s in stars)
+            {
+                sb = new SolidBrush(s.GetColor);
+                r = new Rectangle(s.GetPoint, s.GetSize);
+                e.Graphics.FillEllipse(sb, r);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitStars();
+
+            g = this.CreateGraphics();
+        }
+
+        private void InitStars()
+        {
+            stars = new ArrayList();
+            Star s = new Star(100, 200, 50, 50, 0, Color.FromArgb(100, Color.Red));
+            stars.Add(s);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            func.Rendorter.Rendort(g, stars);
+        }
+    }
+}
---
+++ game1/func/star.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+
+namespace game1.func
+{
+    /// <summary>
+    /// star基类
+    /// </summary>
+    class Star
+    {
+        //位置
+        private int _x, _y;
+
+        //速度
+        private float _speed;
+
+        //颜色
+        private Color _color;
+
+        //大小
+        private int _width, _height;
+
+        public Star(int x, int y, int width, int height, float speed, Color color)
+        {
+            _x = x;
+            _y = y;
+            _speed = speed;
+            _color = color;
+            _width = width;
+            _height = height;
+        }
+
+        public Point GetPoint { get => new Point(_x, _y); }
+        public Color GetColor { get => _color; }
+        public Size GetSize { get => new Size(_width, _height); }
+    }
+
+    static class Rendorter
+    {
+        public static bool isRun;
+        public static void Rendort(Graphics g, ArrayList stars)
+        {
+            if (!isRun)
+                return;
+
+            SolidBrush sb;
+            Rectangle r;
+            foreach (Star ss in stars)
+            {
+                sb = new SolidBrush(ss.GetColor);
+                r = new Rectangle(ss.GetPoint, ss.GetSize);
+                g.Clear(Color.White);
+                g.FillEllipse(sb, r);
+            }
+        }
+    }
+
+}
