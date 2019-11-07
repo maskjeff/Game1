045b09a611888aa0704104e2dbae1ac6423d4492
Mask Zhang <maskjeff@163.com>
2019-11-07 16:52:05 +0800

    Init

--- game1/Form1.Designer.cs
+++ game1/Form1.Designer.cs
@@ -37,7 +37,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 45);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "False";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
